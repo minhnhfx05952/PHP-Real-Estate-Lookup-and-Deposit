e1524f7013615e2dda288e4209bf0357ecec13c6
minhnhfx05952 <minhnhfx05952@funix.edu.vn>
2024-01-03 07:33:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Real Estate Lookup and Deposit Project
+## Note: This is project I made for my customer in FPT Polytechnic Da Nang
+## Description
+This project offers a web application that enables users to look up real estate information and make online deposits.
+
+## Installation Instructions
+
+### Requirements:
+- XAMPP
+- PHPMyAdmin
+- Web Browser
+
+### Step 1: Install XAMPP
+Download and install XAMPP from [here](https://www.apachefriends.org/index.html).
+
+### Step 2: Create a Database
+- Start XAMPP and open PHPMyAdmin.
+- Create a new database for the project.
+
+### Step 3: Install the Application
+- Copy the project files into the `htdocs` folder in the XAMPP installation directory.
+- Access the application via a web browser at `localhost/project_name`.
+
+## Contact and Support
+For any questions or assistance, please contact [Email/Contact Address].
---
+++ footer.php
@@ -0,0 +1,45 @@
+
+      <!-- Footer Start -->
+      <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5">
+                <div class="row g-5">
+                <div class="col-lg-6 col-md-6">
+                            <h5 style="margin-left:46%; margin-top:40px" class="text-white mb-4">Liên lạc</h5>
+                            <p style="margin-left:40%" class="mb-2"><i class="fas fa-user-alt me-3"></i>Nguyễn Văn Nam</p>
+                            <p style="margin-left:40%" class="mb-2"><i class="fa fa-phone-alt me-3"></i>0963382475</p>
+                            <p style="margin-left:40%" class="mb-2"><i class="fa fa-envelope me-3"></i>ngvannam3107@gmail.com</p>
+                            <div style="margin-left:44%;" class="d-flex pt-6">
+                                <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-youtube"></i></a>
+                                <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                        </div>
+                    <div class="col-lg-6 col-md-6">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15335.210753523952!2d108.16996!3d16.0757256!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314219b509a30cfb%3A0xb93a05e26b3b28ff!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1701814208043!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="tel:+84397433097" class="btn btn-lg btn-primary btn-lg-square back-to-top single"><i class="fa-solid fa-phone-flip"></i></a>
+
+
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="Customer/lib/wow/wow.min.js"></script>
+    <script src="Customer/lib/easing/easing.min.js"></script>
+    <script src="Customer/lib/waypoints/waypoints.min.js"></script>
+    <script src="Customer/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="Customer/js/sweetalert.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    
+                        
+    <!-- Template Javascript -->
+    <script src="Customer/js/main.js"></script>
